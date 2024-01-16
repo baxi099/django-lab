9b7514601979ef603c74a93bb6d128ce42b6cb04
baxi099 <jatayubaxi.ce@ddu.ac.in>
2024-01-16 14:07:25 +0530

    Add files via upload

---
+++ registration_form_demo/registration/myapp/views.py
@@ -0,0 +1,13 @@
+# views.py
+from django.shortcuts import render
+from .forms import RegistrationForm
+
+def registration_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            return render(request, 'details.html', {'data': data})
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration_form.html', {'form': form})
